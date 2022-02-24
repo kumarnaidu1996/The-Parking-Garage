183927c6ec12601b39a51978a43edf2c1f3cbbe7
Kumar R <75533233+kumarnaidu1996@users.noreply.github.com>
2022-02-24 12:48:05 +0100

    Update and rename MangoDB.py to MongoDB.py

--- MongoDB.py
+++ MongoDB.py
@@ -4,15 +4,15 @@ import pandas as pd
 import numpy as np
 
 
-# Database (MangoDB) class
-class Mangodb:
+# Database (MongoDB) class
+class Mongodb:
 
     def __init__(self, db_name='SRH_Berlin_University', table_name='Parking_Data'):
         self.db_name = db_name
         self.table_name = table_name
 
-    # mangoDB database
-    def data_upload_to_mangodb(self, record):
+    # mongoDB database
+    def data_upload_to_mongodb(self, record):
 
         try:
             from pymongo import MongoClient
